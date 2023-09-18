40f899b76837cdd4ab5c93046219326bd94c51bc
Olegk2004 <olegk_wot@mail.ru>
2023-09-18 11:41:01 +0300

    vdvdvd

---
+++ src/Lab21/ArrayToList.java
@@ -0,0 +1,36 @@
+package Lab21;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+public class ArrayToList {
+
+    public static List<String> arrayToList(String[] array) {
+        List<String> list = new ArrayList<>(Arrays.asList(array));
+        return list;
+    }
+
+    public static List<Integer> arrayToList(Integer[] array) {
+        List<Integer> list = new ArrayList<>(Arrays.asList(array));
+        return list;
+    }
+
+    public static void main(String[] args) {
+        String[] stringArray = {"apple", "banana", "cherry"};
+        List<String> stringList = arrayToList(stringArray);
+
+        System.out.println("List of Strings:");
+        for (String str : stringList) {
+            System.out.println(str);
+        }
+
+        Integer[] intArray = {1, 2, 3, 4, 5};
+        List<Integer> intList = arrayToList(intArray);
+
+        System.out.println("List of Integers:");
+        for (Integer num : intList) {
+            System.out.println(num);
+        }
+    }
+}
