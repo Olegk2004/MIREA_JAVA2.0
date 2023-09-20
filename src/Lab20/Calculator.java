482b30c30ccd7209f191eed5414291bd773c3246
Vitaly <onebeattrue@gmail.com>
2023-09-20 23:04:10 +0300

    Lab 20 Calculator

--- src/Lab20/Calculator.java
+++ src/Lab20/Calculator.java
@@ -1,47 +1,23 @@
 package Lab20;
 
-public class Calculator<T> {
-    public static <T extends Number> T sum(T a, T b) {
-        if (a instanceof Integer) {
-            return (T) Integer.valueOf(a.intValue() + b.intValue());
-        } else if (a instanceof Double) {
-            return (T) Double.valueOf(a.doubleValue() + b.doubleValue());
-        } else {
-            System.out.println("Неправильный тип данных");
-            return (T) Integer  .valueOf("0");
-        }
-    }
-    public static <T extends Number> T mult(T a, T b) {
-        if (a instanceof Integer) {
-            return (T) Integer.valueOf(a.intValue() * b.intValue());
-        } else if (a instanceof Double) {
-            return (T) Double.valueOf(a.doubleValue() * b.doubleValue());
-        } else {
-            System.out.println("Неправильный тип данных");
-            return (T) Integer.valueOf("0");
-        }
-    }
-    public static <T extends Number> T div(T a, T b) {
-        if (a instanceof Integer) {
-            return (T) Integer.valueOf(a.intValue() / b.intValue());
-        } else if (a instanceof Double) {
-            return (T) Double.valueOf(a.doubleValue() / b.doubleValue());
-        } else {
-            System.out.println("Неправильный тип данных");
-            return (T) Integer.valueOf("0");
-        }
+public class Calculator {
+    public static <T, R> double sum(T a, R b) {
+        return convertType(a) + convertType(b);
     }
 
-    public static <T extends Number> T minus(T a, T b) {
-        if (a instanceof Integer) {
-            return (T) Integer.valueOf(a.intValue() - b.intValue());
-        } else if (a instanceof Double) {
-            return (T) Double.valueOf(a.doubleValue() - b.doubleValue());
-        } else {
-            System.out.println("Неправильный тип данных");
-            return (T) Integer.valueOf("0");
-        }
+    public static <T, R> double mult(T a, R b) {
+        return convertType(a) * convertType(b);
     }
+
+    public static <T, R> double div(T a, R b) {
+        if (convertType(b) == 0) throw new RuntimeException("Division by 0");
+        return convertType(a) / convertType(b);
+    }
+
+    public static <T, R> double minus(T a, R b) {
+        return convertType(a) - convertType(b);
+    }
+
     public static void main(String[] args) {
         Integer[] intArray = {1, 3, 5, 7, 2};
         MinMax<Integer> intMinMax = new MinMax<>(intArray);
@@ -53,7 +29,7 @@ public class Calculator<T> {
         System.out.println("Min: " + doubleMinMax.findMin());
         System.out.println("Max: " + doubleMinMax.findMax());
 
-        int num1 = 10;
+        String num1 = "10";
         int num2 = 5;
         System.out.println("Sum: " + sum(num1, num2));
         System.out.println("Multiply: " + mult(num1, num2));
@@ -66,5 +42,15 @@ public class Calculator<T> {
         System.out.println("Multiply: " + mult(num3, num4));
         System.out.println("Divide: " + div(num3, num4));
         System.out.println("Minus: " + minus(num3, num4));
+    }
+
+    private static <T> double convertType(T value) {
+        if (value instanceof String) {
+            return Double.parseDouble((String) value);
+        } else if (value instanceof Number) {
+            return ((Number) value).doubleValue();
+        } else {
+            throw new RuntimeException("Unknown type");
+        }
     }
 }
